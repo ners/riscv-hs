6a47e0e22c9d3baf3eee1f8da9f3a700d1d654f7
ners <ners@gmx.ch>
2022-10-19 13:45:36 +0200

    Add Mob and Haskell tools to the dev shell
    Co-authored-by: fricklerhandwerk <valentin@fricklerhandwerk.de>

--- flake.nix
+++ flake.nix
@@ -2,13 +2,31 @@
   inputs.nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
   inputs.flake-utils.url = github:numtide/flake-utils;
 
-  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
+  outputs = inputs: inputs.flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
     let
-      pkgs = import inputs.nixpkgs { system = system; };
+      mob-overlay = self: super: {
+        mob = super.mob.overrideAttrs (old: rec {
+          version = "4.0.0";
+
+          src = super.fetchFromGitHub {
+            owner = "remotemobprogramming";
+            repo = old.pname;
+            rev = "v${version}";
+            sha256 = "sha256-c6Feex0FGxxOWBEHxe0GqPHv65EwMMdxIeehZUGbl0Q=";
+          };
+        });
+      };
+      pkgs = import inputs.nixpkgs { system = system; overlays = [ mob-overlay ]; };
+      haskellPackages = pkgs.haskellPackages;
     in
     {
       devShells.default = pkgs.mkShell {
-        buildInputs = [ pkgs.fortune pkgs.cowsay pkgs.lolcat ];
+        buildInputs = [
+          pkgs.mob
+          haskellPackages.ghc
+          haskellPackages.cabal-install
+          haskellPackages.haskell-language-server
+        ];
       };
     });
 }
