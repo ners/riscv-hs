a158d873f8fdc37da444d94aee21d17895e42475
kirschmet2 <115047008+kirschmet2@users.noreply.github.com>
2022-11-02 17:32:54 +0100

    Initiate Haskell project
    Co-authored-by: Gennadi Schult <gennadi.schult@me.com>
    Co-authored-by: fricklerhandwerk <valentin@fricklerhandwerk.de>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   inputs.nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
   inputs.flake-utils.url = github:numtide/flake-utils;
+  inputs.flake-compat.url = github:edolstra/flake-compat;
+  inputs.flake-compat.flake = false;
 
   outputs = inputs: inputs.flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
     let
@@ -17,7 +19,7 @@
         });
       };
       pkgs = import inputs.nixpkgs { system = system; overlays = [ mob-overlay ]; };
-      haskellPackages = pkgs.haskellPackages;
+      haskellPackages = pkgs.haskell.packages.ghc92;
     in
     {
       devShells.default = pkgs.mkShell {
