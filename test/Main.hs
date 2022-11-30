6dbb1673dcd06807d837851db156a43ef16f7f06
Gennadi Schult <gennadi.schult@me.com>
2022-11-30 14:36:53 +0100

    add property tests for `not` and `and` bit-wise functions
    now we can test assumptions by running `cabal test`

--- test/Main.hs
+++ test/Main.hs
@@ -1,13 +1,34 @@
 module Main (main) where
 
-import Prelude hiding (and, not)
 import Bits
 import Control.Monad
+import Test.Hspec
+import Test.QuickCheck
+import Prelude hiding (and, not)
+
+instance Arbitrary Bit where
+  arbitrary = elements [I, O]
+
+testNotIdentity :: Bit -> Expectation
+testNotIdentity x = not (not x) `shouldBe` x
+
+testNotDifference :: Bit -> Expectation
+testNotDifference x = not x `shouldNotBe` x
+
+testAnd :: Bit -> Bit -> Expectation
+testAnd a b
+  | a == b = and a b `shouldBe` a
+  | otherwise = and a b `shouldBe` O
 
 main :: IO ()
 main = do
-   unless (nor I I == O) (fail "nor test failed")
-   unless (and I I == I) (fail "and test failed")
-   unless (and O I == O) (fail "and test failed")
-   unless (not O == I) (fail "not test failed")
-   unless (not I == O) (fail "not test failed")
+  hspec $ do
+    describe "Unary NOT function" $ do
+      it "should be identity when applied twice" $
+        property testNotIdentity
+      it "should not have the same output as the input" $
+        property testNotDifference
+    describe "Binary AND function" $ do
+      it "returns the input value if both are the same" $
+        property testAnd
+  unless (nor I I == O) (fail "nor test failed")
