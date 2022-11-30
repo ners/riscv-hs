ac6fea08a3d3f94880747bf2558f25d89d099ab8
Gennadi Schult <gennadi.schult@me.com>
2022-11-30 13:36:54 +0100

    add unit tests for boolean logic

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,13 @@
 module Main (main) where
 
+import Prelude hiding (and, not)
+import Bits
+import Control.Monad
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+   unless (nor I I == O) (fail "nor test failed")
+   unless (and I I == I) (fail "and test failed")
+   unless (and O I == O) (fail "and test failed")
+   unless (not O == I) (fail "not test failed")
+   unless (not I == O) (fail "not test failed")
