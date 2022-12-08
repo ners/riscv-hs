de089c7c5d0045b9ea7ac714b5fb7057f634bed7
Gennadi Schult <gennadi.schult@me.com>
2022-12-08 19:44:01 +0100

    Implement half and full binary adder

---
+++ src/Adders.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Adders where
+
+import Bits
+
+halfAdder :: Bit -> Bit -> (Bit, Bit)
+halfAdder a b =
+  let sum = xor a b
+      carryOut = and a b
+   in (sum, carryOut)
+
+fullAdder :: Bit -> Bit -> Bit -> (Bit, Bit)
+fullAdder a b carryIn =
+  let (sum1, carryOut1) = halfAdder a b
+      (sum2, carryOut2) = halfAdder carryIn sum1
+   in (sum2, or carryOut1 carryOut2)
