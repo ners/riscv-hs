b47eae9849f456da3d80f993fffd99c7f84c7ecd
Gennadi Schult <gennadi.schult@me.com>
2023-02-15 14:19:00 +0100

    make FixedVector an instance of Functor

--- src/FixedVector/Type.hs
+++ src/FixedVector/Type.hs
@@ -1,30 +1,47 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DatatypeContexts #-}
+{-# LANGUAGE TypeFamilies #-}
 
+-- TODO: hide the constructor of FixedVector
 module FixedVector.Type where
 
 import GHC.TypeLits
+import GHC.Generics (Generic)
+import GHC.Exts (IsList(..))
 
--- TODO thies: write some Haddock documentation for these files / datatypes / functions
--- it's a special format for writing documentation in code
--- TODO gennadi: implement Functor
--- |A FixedVector is a list of size elements, stored as _data
+-- |A FixedVector is a list with a type-level size parameter.
 newtype KnownNat size => FixedVector size t = FixedVector
-  { _data :: [t]
-  } deriving (Show, Eq)
+  { elements :: [t]
+  }
+  deriving newtype (Generic, Show, Eq)
 
--- TODO: fromList? IsList?
--- |toList creates a list with the same elements as a given FixedVector
-toList :: KnownNat n => FixedVector n t -> [t]
-toList = _data
+instance IsList (FixedVector n t) where
+  type Item (FixedVector n t) = t
+  -- TODO: what should we do here?
+  fromList _ = undefined
+  toList = elements
+
+instance Functor (FixedVector n) where
+  f `fmap` v = FixedVector { elements = f `fmap` elements v }
+
+-- TODO fricklerhandwerk
+instance Foldable (FixedVector n)
+
+-- TODO gennadi
+instance Traversable (FixedVector n)
+
+-- TODO can we do Applicative?
+-- for Applicative we would need `pure`, `<*>`
 
 -- |empty creates a FixedVector of 0 elements
 empty :: FixedVector 0 t
-empty = FixedVector { _data = [] }
+empty = FixedVector { elements = [] }
 
 -- |singleton creates a FixedVector with a single element
 singleton :: t -> FixedVector 1 t
-singleton v = FixedVector { _data = [v] }
+singleton v = FixedVector { elements = [v] }
 
 -- |prepend a b adds a as an element at the head of b and increases the size of the vector by 1.
 prepend :: KnownNat n => t -> FixedVector n t -> FixedVector (1 + n) t
@@ -38,4 +55,4 @@ prepend a b = singleton a ++# b
   => FixedVector m t
   -> FixedVector n t
   -> FixedVector (m+n) t
-(++#) a b = FixedVector { _data = _data a ++ _data b }
+(++#) a b = FixedVector { elements = elements a <> elements b }
