599cb2fe7d8322f1ecb3c29b7689856de44ff30e
kirschmet2 <Thies.Wilkening@web.de>
2023-04-19 12:53:40 +0200

    revert formatting changes

--- src/Register.hs
+++ src/Register.hs
@@ -1,23 +1,21 @@
 {-# LANGUAGE DataKinds #-}
 
 module Register where
-
+import FixedVector (FixedVector(..), toList)
+import Bit (Bit(..))
 import Adders (fullAdder)
-import Bit (Bit (..))
-import FixedVector (FixedVector (..), fromNumList, toList, tryFromList)
 import Prelude hiding (sum)
 
 type Register = FixedVector 32 Bit
 
 (+) :: Register -> Register -> (Register, Bit)
-r1 + r2 = (FixedVector{elements = sum}, overflow)
-  where
-    (overflow : sum) = reverse $ addBits O (toList r1) (toList r2)
+r1 + r2 = (FixedVector{ elements = sum }, overflow)
+  where (overflow:sum) = reverse $ addBits O (toList r1) (toList r2)
 
 addBits :: Bit -> [Bit] -> [Bit] -> [Bit]
 addBits c [] [] = [c]
 addBits c as [] = addBits O as [c]
 addBits c [] bs = addBits O [c] bs
-addBits c (a : as) (b : bs) = sum : addBits carry as bs
+addBits c (a:as) (b:bs) = sum : addBits carry as bs
   where
-    (sum, carry) = fullAdder a b c
+    (sum,carry) = fullAdder a b c
