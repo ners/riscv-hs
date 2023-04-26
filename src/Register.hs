e798d6134ec960dacd5a3be652975fa7d4597dbf
ners <ners@gmx.ch>
2023-04-26 12:36:13 +0200

    sorry mario, your register addition is in another branch

--- src/Register.hs
+++ src/Register.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE DataKinds #-}
 
 module Register where
+
+import Bit (Bit (..))
 import FixedVector
-import Bit (Bit(..))
-import Adders (fullAdder)
 import Prelude hiding (sum)
 
 type Register = FixedVector 32 Bit
@@ -11,15 +11,3 @@ type Register = FixedVector 32 Bit
 -- create a Register from a list of Bits, padding missing elements with 0 and ignoring surplus elements
 fromList :: [Bit] -> Register
 fromList = fromListWithDefault O
-
-(+) :: Register -> Register -> (Register, Bit)
-r1 + r2 = (FixedVector{ elements = sum }, overflow)
-  where (overflow:sum) = reverse $ addBits O (toList r1) (toList r2)
-
-addBits :: Bit -> [Bit] -> [Bit] -> [Bit]
-addBits c [] [] = [c]
-addBits c as [] = addBits O as [c]
-addBits c [] bs = addBits O [c] bs
-addBits c (a:as) (b:bs) = sum : addBits carry as bs
-  where
-    (sum,carry) = fullAdder a b c
