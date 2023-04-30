61e8e4eeaa9d74cf827c03e72bffda082a0aa1fb
kirschmet2 <Thies.Wilkening@web.de>
2023-04-30 18:18:22 +0200

    add functions for all Bit operations on Registers

--- src/Register.hs
+++ src/Register.hs
@@ -17,16 +17,22 @@ not :: Register -> Register
 not = fmap Bit.not
 
 and :: Register -> Register -> Register
-and x y = fromList (fmap (uncurry Bit.and) (zip (toList x) (toList y)))
+and x y = fromList (uncurry Bit.and `fmap` zip (toList x) (toList y))
 
--- or :: Register -> Register -> Register
---
--- xor :: Register -> Register -> Register
---
--- nand :: Register -> Register -> Register
---
--- nor :: Register -> Register -> Register
---
--- xnor :: Register -> Register -> Register
---
--- implies :: Register -> Register -> Register
+or :: Register -> Register -> Register
+or x y = fromList (uncurry Bit.or `fmap` zip (toList x) (toList y))
+
+xor :: Register -> Register -> Register
+xor x y = fromList (uncurry Bit.xor `fmap` zip (toList x) (toList y))
+
+nand :: Register -> Register -> Register
+nand x y = fromList (uncurry Bit.nand `fmap` zip (toList x) (toList y))
+
+nor :: Register -> Register -> Register
+nor x y = fromList (uncurry Bit.nor `fmap` zip (toList x) (toList y))
+
+xnor :: Register -> Register -> Register
+xnor x y = fromList (uncurry Bit.xnor `fmap` zip (toList x) (toList y))
+
+implies :: Register -> Register -> Register
+implies x y = fromList (uncurry Bit.implies `fmap` zip (toList x) (toList y))
